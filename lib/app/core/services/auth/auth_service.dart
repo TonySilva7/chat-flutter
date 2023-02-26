419cbf82b5b998b3118ac2d5f330de48053ec582
TonySilva7 <tony.deve7oper@gmail.com>
2023-02-26 16:05:55 -0300

    feat: create Stream to login

--- lib/app/core/services/auth/auth_service.dart
+++ lib/app/core/services/auth/auth_service.dart
@@ -5,7 +5,7 @@ import 'package:chat/app/core/models/chat_user.dart';
 abstract class AuthService {
   ChatUser? get currentUser;
 
-  Stream<ChatUser?> get authStateChanges;
+  Stream<ChatUser?> get userChanges;
 
   Future<void> signup(String name, String email, String password, File image);
   Future<void> login(String email, String password);
--- lib/app/core/services/auth/auth_service_impl.dart
+++ lib/app/core/services/auth/auth_service_impl.dart
@@ -1,32 +1,53 @@
+import 'dart:async';
+
 import 'package:chat/app/core/models/chat_user.dart';
 import 'dart:io';
 
 import 'package:chat/app/core/services/auth/auth_service.dart';
 
 class AuthServiceImpl implements AuthService {
-  @override
-  // TODO: implement authStateChanges
-  Stream<ChatUser?> get authStateChanges => throw UnimplementedError();
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+
+  static final _userStream = Stream<ChatUser>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller!.add(_currentUser);
+  }
 
   @override
-  // TODO: implement currentUser
-  ChatUser? get currentUser => throw UnimplementedError();
+  Stream<ChatUser?> get userChanges => _userStream;
+
+  @override
+  ChatUser? get currentUser => _currentUser;
 
   @override
   Future<void> login(String email, String password) async {
-    // TODO: implement login
-    throw UnimplementedError();
+    _updateUser(_users[email]);
   }
 
   @override
   Future<void> logout() async {
-    // TODO: implement logout
-    throw UnimplementedError();
+    _updateUser(null);
   }
 
   @override
   Future<void> signup(String name, String email, String password, File image) async {
-    // TODO: implement signup
-    throw UnimplementedError();
+    final seedId = DateTime.now();
+
+    final user = ChatUser(
+      id: seedId.millisecondsSinceEpoch.toRadixString(16),
+      name: name,
+      email: email,
+      imageUrl: image.path,
+    );
+
+    _users.putIfAbsent(email, () => user);
+    _updateUser(user);
   }
 }
