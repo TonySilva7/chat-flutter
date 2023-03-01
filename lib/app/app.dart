be2a55584c0471c7338f6d5f58ba49dd5f8e0bbe
TonySilva7 <tony.deve7oper@gmail.com>
2023-03-01 11:51:36 -0300

    refactor: rename push_notifcations_service

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,7 @@
+import 'package:chat/app/core/services/notification/chat_notification_service.dart';
 import 'package:chat/app/pages/auth_or_app_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,14 +9,19 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (ctx) => ChatNotificationService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const AuthOrAppPage(),
       ),
-      home: AuthOrAppPage(),
     );
   }
 }
